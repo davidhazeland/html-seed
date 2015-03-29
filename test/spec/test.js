578000318997711f5a35a0d864a1b0bd31ba4786
davidhazeland <davidhazeland@gmail.com>
2015-03-29 21:02:56 +0700

    Initial

---
+++ test/spec/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* global define, description, it, expect */
+
+define([], function() {
+	describe('test', function() {
+		it('should be pass', function() {
+			expect(true).toEqual(true);
+		});
+	});
+});
